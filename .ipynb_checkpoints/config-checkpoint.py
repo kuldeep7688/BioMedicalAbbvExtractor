96d49c6f3fbfcb85976ade10e7c4295f225b80a3
kuldeep7688 <7688kuldeepsingh@gmail.com>
2020-04-11 01:31:29 +0530

    restructured files and added .gitignore

--- .ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,3 +0,0 @@
-IILEGAL_SHORTFORMS = [
-	'(Group I)', '(Group II)', '(Group III)', '(Group IV)', '(Group V)', '(Group VI)', '(Group VII)', '(Group VIII)', '(Group IX)', '(Group X)', '(Group XI)'
-] 
